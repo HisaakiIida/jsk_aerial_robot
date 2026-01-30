e17c0251c2216a50a9492a47f2290310ea2a5f6d
HisaakiIida <iida@dragon.t.u-tokyo.ac.jp>
2026-01-30 19:33:50 +0800

    [Gryllo] modified yamls, imu message

--- aerial_robot_estimation/src/sensor/imu.cpp
+++ aerial_robot_estimation/src/sensor/imu.cpp
@@ -124,8 +124,9 @@ namespace sensor_plugin
   {
     if(imu_stamp_.toSec() <= prev_time.toSec())
       {
-        ROS_WARN("IMU: bad timestamp. curr time stamp: %f, prev time stamp: %f",
-                 imu_stamp_.toSec(), prev_time.toSec());
+	// Modified!!
+        // ROS_WARN("IMU: bad timestamp. curr time stamp: %f, prev time stamp: %f",
+        //          imu_stamp_.toSec(), prev_time.toSec());
         return;
       }
 
